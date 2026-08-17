577496a361a00814c39789672e293e70112bdb94
DavidMatias44 <daosmaru3667@gmail.com>
2026-08-17 13:59:30 -0600

    add script to establish a connection with the database

---
+++ src/database.py
@@ -0,0 +1,22 @@
+import logging
+
+import psycopg2
+from psycopg2.extensions import connection
+
+logger = logging.getLogger(__name__)
+
+
+def create_connection(params: dict[str, str]) -> connection:
+    logger.debug(
+        "Establishing connection to database with host: %s, port: %s",
+        params.get("host"),
+        params.get("port"),
+    )
+    conn = psycopg2.connect(
+        dbname=params.get("dbname"),
+        user=params.get("user"),
+        password=params.get("password"),
+        host=params.get("host"),
+        port=params.get("port"),
+    )
+    return conn
