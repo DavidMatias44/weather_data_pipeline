09fcb099c54bb8ec4833d0908f5cf48d9383d592
DavidMatias44 <daosmaru3667@gmail.com>
2026-08-27 14:13:53 -0600

    add initial mart models

---
+++ dbt/weather_data_pipeline/models/mart/week_weather_report.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select
+    time::date as date_day,
+    latitude,
+    longitude,
+    elevation,
+    round(avg(temperature::numeric), 2) as avg_temperature,
+    min(temperature) as min_temperature,
+    max(temperature) as max_temperature,
+    round(avg(precip_prob), 2) as avg_precip_prob,
+    min(precip_prob) as min_precip_prob,
+    max(precip_prob) as max_precip_prob
+from {{ ref('stg_weather_readings') }}
+where time::date between current_date - interval '7' day and current_date
+group by 1, 2, 3, 4
