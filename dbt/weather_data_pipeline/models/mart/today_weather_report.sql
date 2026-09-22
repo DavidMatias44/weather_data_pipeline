a3f6f7a5206253786c9775efc3f7a997db5a850f
DavidMatias44 <daosmaru3667@gmail.com>
2026-09-22 12:33:48 -0600

    update mart models to include historical data
    each mart model includes current and two previous forecasts

--- dbt/weather_data_pipeline/models/mart/today_weather_report.sql
+++ dbt/weather_data_pipeline/models/mart/today_weather_report.sql
@@ -5,6 +5,7 @@
 }}
 
 select
+    hour_,
     latitude,
     longitude,
     elevation,
@@ -12,9 +13,25 @@ select
     temp_categorized,
     precip_prob,
     precip_prob_categorized,
-    to_char(time, 'HH24:MI') as hour_
-from {{ ref('int_weather_enriched') }}
-where
-    time::date = {{ current_date_timezone() }}
-    and is_current = 1
-order by extract(hour from time)
+    valid_from,
+    valid_to,
+    is_current
+from (
+    select
+        latitude,
+        longitude,
+        elevation,
+        temperature,
+        temp_categorized,
+        precip_prob,
+        precip_prob_categorized,
+        valid_from,
+        valid_to,
+        is_current,
+        row_number() over (partition by time order by valid_to desc) as rn,
+        to_char(time, 'hh24:mi') as hour_
+    from {{ ref('int_weather_enriched') }}
+    where time::date = {{ current_date_timezone() }}
+) as t1
+where rn <= 3
+order by hour_ asc, rn desc
--- dbt/weather_data_pipeline/models/mart/week_weather_report.sql
+++ dbt/weather_data_pipeline/models/mart/week_weather_report.sql
@@ -5,19 +5,32 @@
 }}
 
 select
+    rn,
     time::date as date_day,
     latitude,
     longitude,
     elevation,
-    round(avg(temperature::numeric), 2) as avg_temperature,
+    is_current,
     min(temperature) as min_temperature,
+    round(avg(temperature::numeric), 2) as avg_temperature,
     max(temperature) as max_temperature,
-    round(avg(precip_prob), 2) as avg_precip_prob,
     min(precip_prob) as min_precip_prob,
+    round(avg(precip_prob), 2) as avg_precip_prob,
     max(precip_prob) as max_precip_prob
-from {{ ref('int_weather_enriched') }}
-where
-    time::date between {{ current_date_timezone() }}
-    and {{ current_date_timezone() }} + interval '7' day
-    and is_current = 1
-group by 1, 2, 3, 4
+from (
+    select
+        time,
+        latitude,
+        longitude,
+        elevation,
+        temperature,
+        precip_prob,
+        is_current,
+        row_number() over (partition by time order by valid_to desc) as rn
+    from {{ ref('int_weather_enriched') }}
+    where
+        time::date between {{ current_date_timezone() }}
+        and {{ current_date_timezone() }} + interval '7' day
+) as t1
+where rn <= 3
+group by 2, 1, 3, 4, 5, 6
